e15e1f0a2161b491e0ff5a8b3761f2b8ec076210
isaijosee <isaijoseee21@gmail.com>
2022-10-13 10:25:15 -0500

    mi segundo commit

--- admin/config/conexion.php
+++ admin/config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "bdsitio");
+$conn = new mysqli("byxumxop92ykjsjhzzlt-mysql.services.clever-cloud.com", "uh5ndolwkdynxo4a", "1gDuGISczeHq5MhqvQf1", "byxumxop92ykjsjhzzlt");
   
   if ($conn->connect_error) {
     die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
